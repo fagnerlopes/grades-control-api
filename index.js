765fc8ec745d19e0ab1ea35e41c4c5f5be1fa877
Fagner Lopes <fagnernlopes@gmail.com>
2021-03-27 08:13:17 -0300

    Add initial files

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import {promises as fs} from 'fs';
+import gradesRouter from './routes/grade.js'
+const { readFile, writeFile } = fs;
+
+const app = express();
+app.use(express.json());
+
+global.fileName = 'grades.json';
+
+app.use('/grades', gradesRouter);
+app.listen(3000, async () => {  
+  try {
+    await readFile(fileName);
+    console.log('API started');    
+  } catch (err) {
+    const initialJson = {
+      nextId: 1,
+      grades: []
+    }
+    await writeFile(fileName, JSON.stringify(initialJson), null, 2);
+    console.log('API started an json file created');    
+  }
+});
